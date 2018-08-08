2a9e838393fa69c9e4cbfeca4fae92aec87c3d03
mike6321 <33277588+mike6321@users.noreply.github.com>
2018-08-08 18:41:16 +0900

    프래그먼트 기본

---
+++ 04_LHS/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/ListRow.java
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.criminalintent;
+
+
+
+public class ListRow {
+}
---
+++ 04_LHS/SampleFragment/app/src/main/java/fragment/techtown/android/samplefragment/MainFragment.java
@@ -0,0 +1,30 @@
+package fragment.techtown.android.samplefragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main,container,false);
+
+        Button btn = (Button) rootView.findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onFragmentChanged(0);
+            }
+        });
+
+        return rootView;
+    }
+}
